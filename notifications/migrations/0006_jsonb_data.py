5432bc4c545581e2aaef8acfc9588a43c2820d45
Tharshan <tharshan09@gmail.com>
2017-06-19 12:34:06 -0400

    add, copy, remove and rename. to fix jsonfield

--- notifications/migrations/0006_jsonb_data.py
+++ notifications/migrations/0006_jsonb_data.py
@@ -1,32 +1,43 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.db import models, migrations
 from django.utils import timezone
+
 import django.contrib.postgres.fields.jsonb
 import json
 
-def forwards_func(apps, schema_editor):
-    Notification = apps.get_model("notifications", "notification")
-    db_alias = schema_editor.connection.alias
-    nn = Notification.objects.using(db_alias).all()
-    for n in nn:
-        j = json.loads(n.data)
-        n.data = j
-        n.save()
+def tranfer_data(apps, schema_editor):
+	Notification = apps.get_model("notifications", "notification")
+	db_alias = schema_editor.connection.alias
+	nn = Notification.objects.using(db_alias).all()
+	for n in nn:
+		if n.data:
+			j = json.loads(n.data)
+			n.data2 = j
+			n.save()
+
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('notifications', '0003_notification_data'),
+        ('notifications', '0005_auto_20160504_1520'),
     ]
 
     operations = [
-        migrations.AlterField(
+    	migrations.AddField(
+            model_name='notification',
+            name='data2',
+            field=django.contrib.postgres.fields.jsonb.JSONField(null=True, blank=True),
+        ),
+        migrations.RunPython(tranfer_data),
+        migrations.RemoveField(
             model_name='notification',
             name='data',
-            field=django.contrib.postgres.fields.jsonb.JSONField(null=True, blank=True),
-            preserve_default=True
         ),
-        migrations.RunPython(forwards_func),
+        migrations.RenameField(
+            model_name='notification',
+            old_name='data2',
+            new_name='data',
+        ),
+
     ]
